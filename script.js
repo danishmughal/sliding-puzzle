8af2d7f6232f85a328b0acf1a54b1cc29c29fafe
Danish Mughal <mughal.danish@gmail.com>
2017-05-04 02:02:33 -0700

    Styling overhaul + tile mapping tweaks for better extendability

--- script.js
+++ script.js
@@ -4,63 +4,85 @@ document.addEventListener("DOMContentLoaded", game);
 function game() {
 
   // Data structure to hold positions of tiles
+  var baseDistance = 33.5;
   var tileMap = {
     1: {
       position: 1,
-      top: '0',
-      left: '0'
+      top: 0,
+      left: 0
     },
     2: {
       position: 2,
-      top: '0',
-      left: '200'
+      top: 0,
+      left: baseDistance * 1
     },
     3: {
       position: 3,
-      top: '0',
-      left: '400'
+      top: 0,
+      left: baseDistance * 2
     },
     4: {
       position: 4,
-      top: '200',
-      left: '0'
+      top: baseDistance,
+      left: 0
     },
     5: {
       position: 5,
-      top: '200',
-      left: '200'
+      top: baseDistance,
+      left: baseDistance
     },
     6: {
       position: 6,
-      top: '200',
-      left: '400'
+      top: baseDistance,
+      left: baseDistance * 2
     },
     7: {
       position: 7,
-      top: '400',
-      left: '0'
+      top: baseDistance * 2,
+      left: 0
     },
     8: {
       position: 8,
-      top: '400',
-      left: '200'
+      top: baseDistance * 2,
+      left: baseDistance
     },
     empty: {
       position: 9,
-      top: '400',
-      left: '400'
+      top: baseDistance * 2,
+      left: baseDistance * 2
     }
+  }
+
+  // Movement map
+  function movementMap(emptyPosition) {
+    if (emptyPosition == 9) return [6, 8];
+    if (emptyPosition == 8) return [5, 7, 9];
+    if (emptyPosition == 7) return [4, 8];
+    if (emptyPosition == 6) return [3, 5, 9];
+    if (emptyPosition == 5) return [2, 4, 6, 8];
+    if (emptyPosition == 4) return [1, 5, 7];
+    if (emptyPosition == 3) return [2, 6];
+    if (emptyPosition == 2) return [1, 3, 5];
+    if (emptyPosition == 1) return [2, 4];
   }
 
   // Board setup according to the tileMap
   var tiles = document.querySelectorAll('.tile');
+  var delay = -50;
   for(var i = 0; i < tiles.length; i++) {
     tiles[i].addEventListener('click', tileClicked ,true );
 
     var tileId = tiles[i].innerHTML;
+    delay += 50;
+    setTimeout(setup, delay, tiles[i]);
+  }
 
-    tiles[i].style.left = tileMap[tileId].left + 'px';
-    tiles[i].style.top = tileMap[tileId].top + 'px';
+  function setup(tile) {
+    console.log(tile);
+    var tileId = tile.innerHTML;
+    tile.style.left = tileMap[tileId].left + '%';
+    tile.style.top = tileMap[tileId].top + '%';
+    recolorTile(tile, tileId);
   }
 
   function tileClicked(event) {
@@ -93,11 +115,14 @@ function game() {
     tileMap.empty.left = tileMap[tileNumber].left;
     tileMap.empty.position = tileMap[tileNumber].position;
 
-    tile.style.top = emptyTop  + 'px'; 
-    tile.style.left = emptyLeft  + 'px';
+    tile.style.top = emptyTop  + '%'; 
+    tile.style.left = emptyLeft  + '%';
     tileMap[tileNumber].top = emptyTop;
     tileMap[tileNumber].left = emptyLeft;
     tileMap[tileNumber].position = emptyPosition;
+
+    recolorTile(tile, tileNumber);
+    console.log(tileMap);
   }
 
 
@@ -108,8 +133,8 @@ function game() {
 
     var leftDifference = selectedTile.left - emptyTile.left
     var topDifference = selectedTile.top - emptyTile.top
-
-    return (Math.abs(leftDifference + topDifference) == 200);
+    console.log(Math.abs(leftDifference + topDifference));
+    return (Math.abs(leftDifference + topDifference) == baseDistance);
   }
 
   // Returns true/false based on if the puzzle has been solved
@@ -124,9 +149,19 @@ function game() {
     return true;
   }
 
+  // Check if tile is in correct place!
+  function recolorTile(tile, tileId) {
+    console.log(tileMap[tileId].position);
+    console.log(tileId);
+    if (tileId == tileMap[tileId].position) {
+      tile.classList.remove("error");
+    } else {
+      tile.classList.add("error");
+    }
+  }
+
   // Shuffles the current tiles
   function shuffle() {
-
   }
 
 

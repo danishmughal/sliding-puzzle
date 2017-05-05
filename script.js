25a15da9e2e47274370688fff253b30ec0da7366
Danish Mughal <mughal.danish@gmail.com>
2017-05-05 01:54:09 -0700

    Basic shuffler and solver working

--- script.js
+++ script.js
@@ -7,41 +7,49 @@ function game() {
   var baseDistance = 33.5;
   var tileMap = {
     1: {
+      tileNumber: 1,
       position: 1,
       top: 0,
       left: 0
     },
     2: {
+      tileNumber: 2,
       position: 2,
       top: 0,
       left: baseDistance * 1
     },
     3: {
+      tileNumber: 3,
       position: 3,
       top: 0,
       left: baseDistance * 2
     },
     4: {
+      tileNumber: 4,
       position: 4,
       top: baseDistance,
       left: 0
     },
     5: {
+      tileNumber: 5,
       position: 5,
       top: baseDistance,
       left: baseDistance
     },
     6: {
+      tileNumber: 6,
       position: 6,
       top: baseDistance,
       left: baseDistance * 2
     },
     7: {
+      tileNumber: 7,
       position: 7,
       top: baseDistance * 2,
       left: 0
     },
     8: {
+      tileNumber: 8,
       position: 8,
       top: baseDistance * 2,
       left: baseDistance
@@ -53,21 +61,25 @@ function game() {
     }
   }
 
+  // Array of tileNumbers in order of last moved
+  var history = [];
+
   // Movement map
-  function movementMap(emptyPosition) {
-    if (emptyPosition == 9) return [6, 8];
-    if (emptyPosition == 8) return [5, 7, 9];
-    if (emptyPosition == 7) return [4, 8];
-    if (emptyPosition == 6) return [3, 5, 9];
-    if (emptyPosition == 5) return [2, 4, 6, 8];
-    if (emptyPosition == 4) return [1, 5, 7];
-    if (emptyPosition == 3) return [2, 6];
-    if (emptyPosition == 2) return [1, 3, 5];
-    if (emptyPosition == 1) return [2, 4];
+  function movementMap(position) {
+    if (position == 9) return [6, 8];
+    if (position == 8) return [5, 7, 9];
+    if (position == 7) return [4, 8];
+    if (position == 6) return [3, 5, 9];
+    if (position == 5) return [2, 4, 6, 8];
+    if (position == 4) return [1, 5, 7];
+    if (position == 3) return [2, 6];
+    if (position == 2) return [1, 3, 5];
+    if (position == 1) return [2, 4];
   }
 
   // Board setup according to the tileMap
   document.querySelector('#shuffle').addEventListener('click', shuffle , true);
+  document.querySelector('#solve').addEventListener('click', solve , true);
   var tiles = document.querySelectorAll('.tile');
   var delay = -50;
   for(var i = 0; i < tiles.length; i++) {
@@ -98,7 +110,7 @@ function game() {
 
   // Moves tile to empty spot
   // Returns error message if tile cannot be moved
-  function moveTile(tile) {
+  function moveTile(tile, recordHistory = true) {
     // Check if Tile can be moved 
     // (must be touching empty tile)
     // (must be directly perpendicular to empty tile)
@@ -107,6 +119,12 @@ function game() {
       console.log("Tile " + tileNumber + " can't be moved.");
       return;
     }
+
+    // Push to history
+    if (recordHistory == true){
+      history.push(tileNumber);
+      console.log("wtf");
+    } 
 
     // Swap tile with empty tile
     var emptyTop = tileMap.empty.top;
@@ -131,9 +149,23 @@ function game() {
     var selectedTile = tileMap[tileNumber];
     var emptyTile = tileMap.empty;
 
-    var leftDifference = selectedTile.left - emptyTile.left
-    var topDifference = selectedTile.top - emptyTile.top
-    return (Math.abs(leftDifference + topDifference) == baseDistance);
+    // Old algorithm, a little buggy w/ 4 incorrect cases
+    // var leftDifference = selectedTile.left - emptyTile.left
+    // var topDifference = selectedTile.top - emptyTile.top
+    // return (Math.abs(leftDifference + topDifference) == baseDistance);
+
+    var movableTiles = movementMap(emptyTile.position);
+    console.log(movableTiles);
+    console.log(selectedTile.position);
+    console.log(movableTiles.includes(selectedTile.position));
+    if (movableTiles.includes(selectedTile.position)) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+
   }
 
   // Returns true/false based on if the puzzle has been solved
@@ -157,30 +189,66 @@ function game() {
     }
   }
 
-  // var shuffleCounter = 0;
-  // function shuffleLoop(tile) {
-  //   console.log(tile);
-  //   console.log(shuffleCounter);
-  //   if (tileMovable(tile.innerHTML)) {
-  //     moveTile(tile);
-  //     shuffleCounter += 1;
-  //     return true;
-  //   } 
-  //   return false;
-  // }
+
+  var lastShuffled;
+
+  function shuffleLoop() {
+    console.log("BEGIN");
+    // console.log(tile);
+    var emptyPosition = tileMap.empty.position;
+    var shuffleTiles = movementMap(emptyPosition);
+    var tilePosition = shuffleTiles[Math.floor(Math.floor(Math.random()*shuffleTiles.length))];
+    console.log("shuffle:" + shuffleTiles);
+    console.log("TP:" + tilePosition);
+    console.log("Empty:" + emptyPosition);
+
+    var locatedTile;
+    for(var i = 1; i <= 8; i++) {
+      if (tileMap[i].position == tilePosition) {
+        var locatedTileNumber = tileMap[i].tileNumber;
+        locatedTile = tiles[locatedTileNumber-1];
+      }
+    }
+    if (lastShuffled != locatedTileNumber) {
+      moveTile(locatedTile);
+      lastShuffled = locatedTileNumber;
+    } else {
+      shuffleLoop();
+    }
+
+  }
 
   // Shuffles the current tiles
   function shuffle() {
     var boardTiles = document.querySelectorAll('.tile');
-    var shuffleDelay = -100;
-    // while (shuffleCounter < 2) {
-      // shuffleDelay += 100;
-      // setTimeout(shuffleLoop, shuffleDelay, tile);
-      for (var i = 0; i < 30; i++){ 
-        var tile = boardTiles[Math.floor(Math.random()*boardTiles.length)];
-        moveTile(tile);
-      }
+    var shuffleDelay = 200;
+    shuffleLoop();
+
+    var shuffleCounter = 0;
+    while (shuffleCounter < 20) {
+      shuffleDelay += 200;
+      setTimeout(shuffleLoop, shuffleDelay);
+      shuffleCounter++;
+    }
+      // for (var i = 0; i < 30; i++){ 
+      //   var tile = boardTiles[Math.floor(Math.random()*boardTiles.length)];
+      //   moveTile(tile);
+      // }
   }
+
+  // Temporary function for solving puzzle.
+  // To be reimplemented with a more sophisticated algorithm
+  function solve() {
+    repeater = history.length;
+    for (var i = 0; i < repeater; i++) {
+      console.log(history);
+      console.log("moving");
+      // moveTile(, false);
+      setTimeout(moveTile, i*100, tiles[history.pop()-1], false);
+
+    }
+  }
+
 
 
 }
